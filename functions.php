41b8a1c21fcdf109f789aa5817dc9c2f500209ac
Coby Tamayo <ctamayo@sitecrafting.com>
2016-08-31 14:46:13 -0700

    global debugging functions

--- functions.php
+++ functions.php
@@ -33,4 +33,33 @@ if ( ! class_exists( 'Timber' ) ) {
 $site = new Project\Site();
 $site->configure();
 
+
+/**
+ * Log stuff to wp-content/debug.log, with a prefix so you can pipe
+ * to a grep to filter out irrelevant log entries.
+ * Assumes you have WP debugging configured like so in wp-config.php:
+ *  define('WP_DEBUG',              true);
+ *  define('WP_DEBUG_DISPLAY',      false);
+ *  define('WP_DEBUG_LOG',          true);
+ * @param  mixed $message the message or other value to log. If not a string, gets var_export'ed
+ */
+function debug($message) {
+  if(!is_string($message)) {
+    $message = var_export($message,true);
+  }
+
+  // break up message by line and prefix each one
+  error_log( implode("\n", array_map(function($line) {
+    return "debug $line";
+  }, explode("\n", $message))));
+}
+
+/**
+ * Variadic (sprintf() style) version of debug(). Calls sprintf() internally.
+ */
+function sdebug() {
+  debug(call_user_func_array('sprintf', func_get_args()));
+}
+
+
 ?>
