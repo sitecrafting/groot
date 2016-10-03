c77affde5ed393dbeeb285c8ad416781861dd018
Reena <rhensley@sitecrafting.com>
2016-10-03 13:16:38 -0700

    clean up uncessary prefixes in css

--- js/project/common.js
+++ js/project/common.js
@@ -1,8 +1,12 @@
 (function($) {
-$(document).ready(function() {
 
+	//wrap select elements in div for custom styling
+	$('select').wrap('<div class="selectbox-container"');
+
+	//make sure embedded videos fit the container (are responsive)
 	$('.rtecontent').fitVids();
 
+	//flexslider for default wordpress gallery
 	$('.gallery.flexslider').flexslider({
 		controlNav: 'thumbnails'
 	});
@@ -10,5 +14,5 @@ $(document).ready(function() {
 	// Make nav menu nice & responsive
 	$('nav.main-nav').responsiveNav();
 
-});
+
 })(jQuery);
