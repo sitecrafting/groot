a9c67318547572d61b907e6af46bab1f7618269d
Coby Tamayo <ctamayo@sitecrafting.com>
2018-03-20 14:00:49 -0700

    configure site with conventional SiteCrafting defaults

--- functions.php
+++ functions.php
@@ -2,6 +2,7 @@
 
 use Groot\Autoloader;
 use Groot\PluginManager;
+use Conifer\Post\Image;
 use Conifer\Site;
 
 require_once 'lib/Groot/Autoloader.php';
@@ -27,9 +28,61 @@ if (!$pluginManager->requirements_met()) {
 
 // Build out the site.
 // Put WordPress configurations, such as filter and action hooks,
-// inside the build method.
+// inside the config function passed to Site::configure().
 $site = new Site();
-$site->configure();
+$site->configure(function() {
+  add_theme_support( 'post-thumbnails' );
+  add_theme_support( 'menus' );
+
+  add_action( 'init', ['\Conifer\Admin', 'add_theme_settings_page'] );
+
+  add_filter( 'posts_search', ['\Conifer\AcfSearch', 'advanced_custom_search'], 10, 2 );
+
+  // used for Gallery ACF layout option in flexible content
+  Image::add_size( 'gallery', 900, 600, true );
+
+  //USED FOR Image-Row ACF layout option in flexible content
+  Image::add_size( 'image-row-small', 300, 235, true );
+  Image::add_size( 'image-row-medium', 450, 350, true );
+  Image::add_size( 'image-row-large', 900, 450, true );
+
+  // Make certain custom sizes available in the RTE
+  // use this to unset or add image size options for RTE insert
+  /*add_filter( 'image_size_names_choose', function($sizes) {
+
+    //USE THIS TO UNSET DEFAULT VARIABLE SIZES AND SET YOUR OWN CUSOM SIZES
+    //unset( $sizes['large'] );
+    //unset( $sizes['medium'] );
+    //unset( $sizes['small'] );
+
+    return array_merge( $sizes, [
+      'image-row-small' => __( 'Small 300x235' ),
+      'image-row-medium' => __( 'Medium 450x350' ),
+      'image-row-large' => __( 'Large 900x450' )
+    ]);
+  });*/
+
+  //remove_shortcode( 'gallery' );
+  //Gallery::register( 'gallery' );
+  add_filter( 'use_default_gallery_style', '__return_false' );
+
+  // register common nav menus
+  register_nav_menus([
+    'primary' => 'Main Navigation', // main page/nav structure
+    'global' => 'Global Navigation', // for stuff like social icons
+    'footer' => 'Footer Navigation', // footer links
+  ]);
+
+  //blog sidebar
+  register_sidebar([
+    'name' => 'Blog Filter Bar',
+    'id' => 'blog-filters',
+    'before_widget' => '<div id="%1$s" class="filter %2$s">',
+    'after_widget'  => "</div>\n",
+    'before_title'  => '<h3 class="filtertitle">',
+    'after_title'   => "</h3>\n"
+  ]);
+});
 
 
 
