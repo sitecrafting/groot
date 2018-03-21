85ad7813facfab12a0e8c3050d43d0cd181edf14
Coby Tamayo <acobster@users.noreply.github.com>
2018-03-20 20:32:41 -0700

    new BlogPost, not Post, on single

--- single.php
+++ single.php
@@ -4,8 +4,8 @@
  */
 
 use Timber\Timber;
-use Conifer\Post\Post;
+use Conifer\Post\BlogPost;
 
-$data = $site->get_context_with_post( new Post() );
+$data = $site->get_context_with_post(new BlogPost());
 
 Timber::render( 'single.twig', $data );
