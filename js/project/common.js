5141207ad972d05a20f7119f430f02a6807154d7
Coby Tamayo <ctamayo@sitecrafting.com>
2018-04-13 10:50:10 -0700

    more whitespace/linting edits

--- js/project/common.js
+++ js/project/common.js
@@ -1,28 +1,27 @@
+/* globals jQuery */
 (function($) {
 
-	//main hero slideshow
-	$('.hero.flexslider').flexslider();
+  //main hero slideshow
+  $('.hero.flexslider').flexslider();
 
-	// Make nav menu nice & responsive
-	$('nav.main-nav').responsiveNav({
-		navType: 'offCanvas' //default option for dropdown type use 'dropdown'
-	});
-	$('nav.side-nav').mobileSidenav();
+  // Make nav menu nice & responsive
+  $('nav.main-nav').responsiveNav({
+    navType: 'offCanvas' //default option for dropdown type use 'dropdown'
+  });
+  $('nav.side-nav').mobileSidenav();
 
-	// Custom Select Boxes
-	$('select').wrap('<div class="selectbox-container"></div>');
+  // Custom Select Boxes
+  $('select').wrap('<div class="selectbox-container"></div>');
 
-	// Responsive Videos
-	$('.rtecontent').fitVids();
+  // Responsive Videos
+  $('.rtecontent').fitVids();
 
-	// Accordions
-	$('dl.accordion').accordion();
+  // Accordions
+  $('dl.accordion').accordion();
 
-	// gallery flexslider
-	$('.gallery-slideshow.flexslider').flexslider({
-		controlNav: 'thumbnails'
-	});
-
-
+  // gallery flexslider
+  $('.gallery-slideshow.flexslider').flexslider({
+    controlNav: 'thumbnails'
+  });
 
 })(jQuery);
