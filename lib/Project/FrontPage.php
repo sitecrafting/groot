9de57c74cea7cef4e459ddb366ddb2af8e04f0fe
Coby Tamayo <acobster@gmail.com>
2018-12-14 23:48:39 -0800

    add lib/Project dir

---
+++ lib/Project/FrontPage.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Project\Post\FrontPage class
+ *
+ * @copyright 2018 SiteCrafting, Inc.
+ * @author    Coby Tamayo <ctamayo@sitecrafting.com>
+ */
+
+namespace Project\Post;
+
+use Conifer\Post\FrontPage as ConiferFrontPage;
+
+/**
+ * Theme-specific behavior for the WP home page
+ */
+class FrontPage extends ConiferFrontPage {
+  /*
+   * @groot front_page_methods
+   */
+}
+
+?>
