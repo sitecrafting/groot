06fa1d8e8a5e07765cfe79c99273ad0ad18be968
Coby Tamayo <acobster@gmail.com>
2018-12-22 23:49:49 -0800

    configure babel

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,16 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
         test: /\.(less|css)/,
         use: [
           {
