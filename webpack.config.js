56f8c0e25abdd64f6b33702ec2c2c7865e2d561d
Coby Tamayo <acobster@gmail.com>
2018-12-22 22:53:39 -0800

    configure webpack to compile JS and LESS

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,43 @@
 const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
 
-  entry: './js/src/common.js',
+  entry: {
+    common: './js/src/common.js',
+    style: './less/style.less',
+  },
 
   output: {
-    filename: 'common.min.js',
-    path:     path.resolve(__dirname, 'js/dist'),
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'js/dist'),
+  },
+
+  plugins: [
+    // extract CSS to a separate stylesheet, rather than
+    // bundling into a JS module
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.(less|css)/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: path.resolve(__dirname)
+            },
+          },
+          'css-loader',
+          'less-loader',
+        ],
+      },
+    ],
   },
 
   mode: 'production',
