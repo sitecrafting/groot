b4a6b9fae3cb9a3fdbc5226652fdcc22d28d874a
Coby Tamayo <acobster@gmail.com>
2019-01-15 16:56:04 -0800

    copy style.css from dist into theme directory on compile

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,30 @@ class AssetsVersionPlugin {
   }
 }
 
+// Plugin for copying compiled stylesheets to theme directory
+class CopyDistFilesPlugin {
+  constructor(files) {
+    this.files = files
+  }
+
+  apply() {
+    for (const distFile in this.files) {
+      fs.readFile(distFile, (err, contents) => {
+        if (err) {
+          console.log(`Error reading ${distFile} `, err);
+        } else {
+          const dest = this.files[distFile];
+          fs.writeFile(dest, contents, (err) => {
+            if (err) {
+              console.log('Error writing to ${dest}: ', err);
+            }
+          })
+        }
+      })
+    }
+  }
+}
+
 module.exports = {
 
   entry: {
@@ -33,6 +57,10 @@ module.exports = {
     }),
 
     new AssetsVersionPlugin([]),
+
+    new CopyDistFilesPlugin({
+      'dist/style.css': 'style.css',
+    }),
 
   ],
 
