e0960338dcd315bd044e4cb41d0fa8d73433de80
Reena Hensley <rhensley@sitecrafting.com>
2020-05-08 12:53:07 -0700

    update index page for pulling blog posts for main archive view and category filtering, temporarily commeneting out archive date option

--- index.php
+++ index.php
@@ -9,30 +9,50 @@ use Timber\Timber;
 use Conifer\Post\Page;
 use Conifer\Post\BlogPost;
 
-// Get common/site-wide data
-$data = $site->get_context_with_posts(BlogPost::get_all([
-  'paged'        => get_query_var('paged'),
-  'category__in' => [get_query_var('cat')],
-]));
 
+//blog page
+$newsPage = Page::get_blog_page();
+
+$featurePost = '';
+$is_category = false;
+$catQuery = '';
 
 if( is_tag() || is_category() ) {
-  // tag/category archive page
-  $data['post'] = new TimberTerm();
-  $data['is_category'] = true;
+
+	// tag/category archive page
+	$post = new TimberTerm();
+	$is_category = true;
+
+	$catQuery = get_query_var('cat');
+
 } else {
-  // regular blog archive
-  $data['post'] = Page::get_blog_page();
+
+	// regular blog archive
+	$post = $newsPage;
+
 }
 
 //get archives
-$data['archives'] = new Archives();
-
-//blog page
-$data['newsPage'] = Page::get_blog_page();
+//This is not currently in a functioning state to be able to
+//filter by archive date
+//$archives = new Archives();
 
 //get pagination
-$data['pagination'] = Timber::get_pagination();
+$pagination = Timber::get_pagination();
+
+
+// GET CONTEXT DATA
+$data = $site->context([
+	'newsPage' => $newsPage,
+	'post' => $post,
+	'is_category' => $is_category,
+	'posts' => BlogPost::get_all([
+		'category__in'	=> $catQuery, 
+		'paged'			=> get_query_var('paged'),
+	]),
+	'pagination' => $pagination,
+	//'archives' => $archives
+]);
 
 // Render the archive view
 Timber::render( 'index.twig', $data );
