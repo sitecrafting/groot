cf01143b5c8a0d8891e5d6feee68e66a81577a6d
Leah Ruisenor <lruisenor@sitecrafting.com>
2020-05-29 14:44:27 -0700

    added woocommerce style overide less file

--- functions/woocommerce-shop.php
+++ functions/woocommerce-shop.php
@@ -15,3 +15,16 @@ function new_loop_shop_per_page( $cols ) {
   $cols = 4;
   return $cols;
 }
+
+/**
+ * Woocommerce has a default message that it shows if you tick the box but don’t type any message. 
+ * This filter changes that default message
+ * 
+ * Display a Store Notice (Optional)
+ * To show customers a site-wide Store Notice:
+ * Go to: Appearance > Customize > WooCommerce > Store Notice:
+ */
+//add_filter( 'woocommerce_demo_store', 'wc_custom_store_notice_updated' );
+function wc_custom_store_notice_updated( $text ) {
+	return str_replace( 'This is a demo store for testing purposes &mdash; no orders shall be fulfilled.', '<a href="/shop/">Check out our items.</a>', $text );
+}
