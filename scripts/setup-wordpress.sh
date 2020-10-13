4caf19edbffc75fa7cd9c4efa98e9b05e0aceb5d
Coby Tamayo <ctamayo@sitecrafting.com>
2020-10-12 18:45:35 -0700

    install Timber as a Composer package in the dev environment

--- scripts/setup-wordpress.sh
+++ scripts/setup-wordpress.sh
@@ -116,15 +116,8 @@ EOF
 
   # install/activate plugins and theme
   uninstall_plugins hello akismet
-  wp --quiet plugin install --activate timber-library
   wp --quiet plugin activate conifer
   wp --quiet theme activate groot
-
-  # install a specific version of Timber if necessary
-  if [[ "$TIMBER_VERSION" ]]
-  then
-    composer require --dev timber/timber:"$TIMBER_VERSION"
-  fi
 
   # uninstall stock themes
   wp theme uninstall twentyten twentyeleven twentytwelve \
