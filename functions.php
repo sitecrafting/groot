2e0bc16467355003a4a9d378fad7a9e2fcf53f1d
Reena Hensley <rhensley@sitecrafting.com>
2026-04-03 14:22:02 -0700

    add base functionality for password protected pages

--- functions.php
+++ functions.php
@@ -220,6 +220,27 @@ $site->configure(function() {
         return $buttons;
     }
 
+    /**
+     * Use specific template for password protected posts.
+     *
+     * By default, this will use the **password-protected.php** template file. If you want password
+     * templates specific to a post type, use **password-protected-$posttype.php**.
+     */
+    add_filter('template_include', 'get_password_protected_template', 99);
+
+    function get_password_protected_template($template) {
+        global $post;
+
+        if (!empty($post) && is_singular() && post_password_required($post->ID)) {
+            $template = locate_template([
+                'password-protected.php',
+                "password-protected-{$post->post_type}.php",
+            ]) ?: $template;
+        }
+
+        return $template;
+    };
+
     /** CROP THUMBNAILS PLUGIN - RETINA SUPPORT **/
     /**
      * 
