e7fbb60f3260fa20565420a6625b255f68455eb0
Scott Dunham <sdunham@sitecrafting.com>
2026-07-27 21:37:58 -0700

    GRT-7: Update dev dependencies to install ACF Pro via the SC private packagist, instead of ACF

--- scripts/setup-wordpress.sh
+++ scripts/setup-wordpress.sh
@@ -117,9 +117,8 @@ EOF
   # install/activate plugins and theme
   uninstall_plugins hello akismet
 
-  wp --quiet plugin install --activate advanced-custom-fields
   wp --quiet plugin install --activate classic-editor
-  wp --quiet plugin activate conifer
+  wp --quiet plugin activate advanced-custom-fields-pro
   wp --quiet theme activate groot
 
   # uninstall stock themes
