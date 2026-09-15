1b89967dbaf0024ad0c0ed52831edd8c1a44b20a
Reena Hensley <rhensley@sitecrafting.com>
2026-09-15 14:33:25 -0700

    structural html and pattern updates, better starter styles and some cleanup to start in a better place for custom themes

--- search.php
+++ search.php
@@ -4,11 +4,28 @@
  */
 
 use Timber\Timber;
+use Conifer\Post\Post;
 
 global $wp_query;
 
+//calculate: Showing 1-10 of 603
+if ( !is_paged() ) {
+    // first page of pagination
+    $first_post = absint( $wp_query->get('paged') - 1 );
+    $last_post = $first_post + $wp_query->post_count - 1;
+    $all_posts = $wp_query->found_posts;
+} else {
+    $first_post = absint( $wp_query->get('paged') - 1 ) * $wp_query->get('posts_per_page') + 1;
+    $last_post = $first_post + $wp_query->post_count - 1;
+    $all_posts = $wp_query->found_posts;
+} 
+
+$enumerate_page = 'Showing '. $first_post .' - '. $last_post .' of ' . $all_posts;
+
+
 $data = Timber::context([
     'posts' => Timber::get_posts(),
+    'enumerate_page' => $enumerate_page,
     'search_total' => $wp_query->found_posts
 ]);
 
